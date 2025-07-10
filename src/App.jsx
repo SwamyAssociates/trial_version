7a9c0faffc65098c20e60635e839daa7e04b72a9
Swamy <gowtham.kannappan@yahoo.com>
2025-07-10 22:47:30 +0530

    Fix case mismatch for Footer import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar.jsx';
-import Footer from './components/Footer/Footer.jsx';
+import Footer from './components/Footer/FooterTemp.jsx';
 import EnquiryForm from './components/Contact/EnquiryForm.jsx';
 import FeedbackForm from './components/Contact/FeedbackForm.jsx';
 import Carousel from './components/Carousel/Carousel.jsx';
